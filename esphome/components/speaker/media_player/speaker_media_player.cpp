78fcd04449619de300e43e6007c17752a2555705
Mischa Siekmann <mischa@gnumpi.com>
2025-06-29 14:37:41 +0200

    fix: announcement state got overwritten if media was playing as well

--- esphome/components/speaker/media_player/speaker_media_player.cpp
+++ esphome/components/speaker/media_player/speaker_media_player.cpp
@@ -347,9 +347,8 @@ void SpeakerMediaPlayer::loop() {
     ESP_LOGE(TAG, "The announcement pipeline's audio decoder encountered an error.");
   }
 
-  if (this->announcement_pipeline_state_ != AudioPipelineState::STOPPED) {
-    this->state = media_player::MEDIA_PLAYER_STATE_ANNOUNCING;
-  } else {
+  if (this->announcement_pipeline_state_ == AudioPipelineState::STOPPED) {
+    
     optional<PlaylistItem> next_item;
     if( this->announcement_repeat_one_ && this->curr_announce_item_.has_value() ){
       // repeat current playing_item
@@ -386,62 +385,67 @@ void SpeakerMediaPlayer::loop() {
     } else {
       this->curr_announce_item_.reset();
     }
-  } 
-  
-  {
-      if (this->is_paused_) {
-        this->state = media_player::MEDIA_PLAYER_STATE_PAUSED;
-      } else if (this->media_pipeline_state_ == AudioPipelineState::PLAYING) {
-        this->state = media_player::MEDIA_PLAYER_STATE_PLAYING;
-      } else if (this->media_pipeline_state_ == AudioPipelineState::STOPPED) {
-        
-        optional<PlaylistItem> next_item;
-        if( this->media_repeat_one_ && this->curr_media_item_.has_value() ){
-          // repeat current playing_item
-          next_item = this->curr_media_item_.value();
-        } else if (!this->media_playlist_.empty()){
-          // play front of playlist next
-          next_item = this->media_playlist_.front();
-          this->media_playlist_.pop_front();
-        }
-
-        if(next_item.has_value()){
-          uint32_t timeout_ms = 0;
-          if( this->curr_media_item_.has_value()){
-            // Only delay starting playback if moving on the next playlist item or repeating the current item
-            timeout_ms = this->announcement_playlist_delay_ms_;
-          }
-          printf( "Playing next item after pipeline has stopped\n" );
-          this->curr_media_item_ = next_item.value();
-          if( next_item.value().url.has_value()){
-#if USE_SNAPCAST                
-            if( this->snapcast_client_ && this->snapcast_client_->is_snapcast_url(next_item.value().url.value())){
-              this->snapcast_client_->connect_to_url(next_item.value().url.value());
-              this->media_pipeline_->start_snapcast( this->snapcast_client_->get_stream() );
-            } 
-            else 
-#endif               
-            {
-              this->media_pipeline_->start_url(next_item.value().url.value());
-            }
-          } else if (next_item.value().file.has_value()) {
-              this->media_pipeline_->start_file(next_item.value().file.value());
-          }
-          if (timeout_ms > 0) {
-              // Pause pipeline internally to facilitate the delay between items
-              this->media_pipeline_->set_pause_state(true);
-              // Internally unpause the pipeline after the delay between playlist items, if the media player state is
-              // not paused.
-              this->set_timeout("next_media", timeout_ms,
-                                [this]() { this->media_pipeline_->set_pause_state(this->is_paused_); });
-          }
-        } else {
-          this->curr_media_item_.reset();
-          this->state = media_player::MEDIA_PLAYER_STATE_IDLE;
-        }
+    
+    // announcement is not running at this stage
+    // only start new media track if no announcement is running
+    // (otherwise MEDIA_PLAYER_STATE_ANNOUNCING get's overwritten and voice assistant can't detect end of announcement)
+    
+    if (this->is_paused_) {
+      this->state = media_player::MEDIA_PLAYER_STATE_PAUSED;
+    } else if (this->media_pipeline_state_ == AudioPipelineState::PLAYING) {
+      this->state = media_player::MEDIA_PLAYER_STATE_PLAYING;
+    } else if (this->media_pipeline_state_ == AudioPipelineState::STOPPED) {
+      
+      optional<PlaylistItem> next_item;
+      if( this->media_repeat_one_ && this->curr_media_item_.has_value() ){
+        // repeat current playing_item
+        next_item = this->curr_media_item_.value();
+      } else if (!this->media_playlist_.empty()){
+        // play front of playlist next
+        next_item = this->media_playlist_.front();
+        this->media_playlist_.pop_front();
       }
-  }
 
+      if(next_item.has_value()){
+        uint32_t timeout_ms = 0;
+        if( this->curr_media_item_.has_value()){
+          // Only delay starting playback if moving on the next playlist item or repeating the current item
+          timeout_ms = this->announcement_playlist_delay_ms_;
+        }
+        printf( "Playing next item after pipeline has stopped\n" );
+        this->curr_media_item_ = next_item.value();
+        if( next_item.value().url.has_value()){
+#if USE_SNAPCAST                
+          if( this->snapcast_client_ && this->snapcast_client_->is_snapcast_url(next_item.value().url.value())){
+            this->snapcast_client_->connect_to_url(next_item.value().url.value());
+            this->media_pipeline_->start_snapcast( this->snapcast_client_->get_stream() );
+          } 
+          else 
+#endif               
+          {
+            this->media_pipeline_->start_url(next_item.value().url.value());
+          }
+        } else if (next_item.value().file.has_value()) {
+            this->media_pipeline_->start_file(next_item.value().file.value());
+        }
+        if (timeout_ms > 0) {
+            // Pause pipeline internally to facilitate the delay between items
+            this->media_pipeline_->set_pause_state(true);
+            // Internally unpause the pipeline after the delay between playlist items, if the media player state is
+            // not paused.
+            this->set_timeout("next_media", timeout_ms,
+                              [this]() { this->media_pipeline_->set_pause_state(this->is_paused_); });
+        }
+      } else {
+        this->curr_media_item_.reset();
+        this->state = media_player::MEDIA_PLAYER_STATE_IDLE;
+      }
+    }
+  
+  } else { // this->announcement_pipeline_state_ != AudioPipelineState::STOPPED
+    this->state = media_player::MEDIA_PLAYER_STATE_ANNOUNCING;
+  }
+  
   if (this->state != old_state) {
     this->publish_state();
     ESP_LOGD(TAG, "State changed to %s", media_player::media_player_state_to_string(this->state));
